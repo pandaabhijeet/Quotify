4cb1d194cfdee2f5aeb968a779a3711b32cd7bc5
chronicle <abhijeetpanda06@gmail.com>
2022-10-02 10:02:54 +0530

    SplashScreen Activity - Added functionality for SplashScreen,logo and title.

---
+++ app/src/main/java/com/example/quotify/SplashScreenActivity.java
@@ -0,0 +1,45 @@
+package com.example.quotify;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    //private ImageView splashLogoView;
+    private TextView splashTitleView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+        {
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        }
+
+        //splashLogoView = findViewById(R.id.logo_splash);
+        splashTitleView = findViewById(R.id.title_splash);
+
+        Typeface typeface = Typeface.createFromAsset(getAssets(),"fonts/crimson_italic.ttf");
+        splashTitleView.setTypeface(typeface);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run()
+            {
+                Intent loginIntent = new Intent(SplashScreenActivity.this,MainActivity.class);
+                startActivity(loginIntent);
+            }
+        },2000);
+
+    }
+}
